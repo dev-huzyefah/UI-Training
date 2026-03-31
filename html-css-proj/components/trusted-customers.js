48b5939b0fd863edb41b941e93b3dfd6a03f76b1
Huzyefah <huzyefah.saqib@devsinc.com>
2026-03-31 11:02:53 -0700

    applied identified fixes

--- html-css-proj/components/trusted-customers.js
+++ html-css-proj/components/trusted-customers.js
@@ -5,20 +5,22 @@ class TrustedCustomers extends HTMLElement {
         <h4 class="text-center title-2 mb-4">Trusted By</h4>
         <div class="container mb-5">
           <div class="flexitems">
-            <div class="flexitem d-none d-lg-block d-xl-block d-md-block">
+            <div class="flexitem">
               <img src="./assets/customer-logo-1.png" />
             </div>
-            <div class="flexitem d-none d-lg-block d-xl-block">
+            <div class="flexitem">
               <img src="./assets/customer-logo-2.png" />
             </div>
-            <div class="flexitem d-none d-lg-block d-xl-block d-md-block">
+            <div class="flexitem">
               <img src="./assets/customer-logo-3.png" />
             </div>
-            <div class="flexitem"><img src="./assets/customer-logo-4.png" /></div>
-            <div class="flexitem d-none d-lg-block d-xl-block">
+            <div class="flexitem">
+              <img src="./assets/customer-logo-4.png" />
+            </div>
+            <div class="flexitem">
               <img src="./assets/customer-logo-5.png" />
             </div>
-            <div class="flexitem d-none d-lg-block d-xl-block lst-img">
+            <div class="flexitem">
               <img src="./assets/customer-logo-6.png" />
             </div>
           </div>
