d7796043980622d89ee61ec10b827f6a20c4e297
Huzyefah <huzyefah.saqib@devsinc.com>
2026-04-15 19:35:18 +0500

    Amends

---
+++ react-proj/api/server.js
@@ -0,0 +1,21 @@
+
+import path from 'path';
+import jsonServer from 'json-server';
+import { fileURLToPath } from "url";
+
+const server = jsonServer.create();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Path to your db.json - Vercel will bundle this if it's referenced
+const router = jsonServer.router(path.join(__dirname, '../db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+// Optional: Rewriting /api/* to /* so json-server handles it correctly
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1'
+}));
+server.use(router);
+
+module.exports = server;
