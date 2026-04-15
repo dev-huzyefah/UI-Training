734a9205931345a24e4321757592a358646125e1
Huzyefah <huzyefah.saqib@devsinc.com>
2026-04-15 19:04:32 +0500

    .

--- react-proj/setup-db.js
+++ react-proj/setup-db.js
@@ -2,6 +2,10 @@
 
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const dbPath = path.join(__dirname, "db.json");
 const examplePath = path.join(__dirname, "db.example.json");
