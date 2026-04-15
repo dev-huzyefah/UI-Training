34963fa1d130478aa08353a3a2cba74f0900e299
Huzyefah <huzyefah.saqib@devsinc.com>
2026-04-15 18:14:36 +0500

    es module error fix

--- react-proj/setup-db.js
+++ react-proj/setup-db.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
 const dbPath = path.join(__dirname, "db.json");
 const examplePath = path.join(__dirname, "db.example.json");
